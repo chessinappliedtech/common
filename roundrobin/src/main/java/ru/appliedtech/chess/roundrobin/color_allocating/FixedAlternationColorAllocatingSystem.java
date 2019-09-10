5f19eaed7340c054d416845d4bf081d6b029f5cc
Ilya Maltsev <ilya.maltsev@reltio.com>
2019-09-10 15:31:32 +0500

    Поправлена ошибка в назначении цвета при двух добавленных игроках

--- roundrobin/src/main/java/ru/appliedtech/chess/roundrobin/color_allocating/FixedAlternationColorAllocatingSystem.java
+++ roundrobin/src/main/java/ru/appliedtech/chess/roundrobin/color_allocating/FixedAlternationColorAllocatingSystem.java
@@ -2,6 +2,7 @@ package ru.appliedtech.chess.roundrobin.color_allocating;
 
 import ru.appliedtech.chess.Color;
 
+import java.text.MessageFormat;
 import java.util.*;
 
 import static java.util.stream.Collectors.toList;
@@ -62,6 +63,23 @@ public class FixedAlternationColorAllocatingSystem implements ColorAllocatingSys
                     isWhite = !isWhite;
                 }
             }
+            for (String joinedPlayer1 : joinedPlayers) {
+                for (String joinedPlayer2 : joinedPlayers) {
+                    List<Color> colors = new ArrayList<>();
+                    for (int i = 0; i < maxGames; i++) {
+                        colors.add(isWhite ? Color.white : Color.black);
+                        isWhite = !isWhite;
+                    }
+                    Pair pair = new Pair(joinedPlayer1, joinedPlayer2, colors);
+                    pairs.add(pair);
+                    storeColorCounts(colorCounts, joinedPlayer1, colors);
+                    Pair pair2 = new Pair(joinedPlayer2, joinedPlayer1, colors.stream().map(Color::revert).collect(toList()));
+                    pairs.add(pair2);
+                }
+                if (joinedPlayers.size() % 2 == 0) {
+                    isWhite = !isWhite;
+                }
+            }
         } else {
             for (String playerId : initialPlayers) {
                 isWhite = colorCounts.get(playerId).isMoreBlacks();
@@ -75,6 +93,21 @@ public class FixedAlternationColorAllocatingSystem implements ColorAllocatingSys
                     pairs.add(pair);
                     storeColorCounts(colorCounts, playerId, colors);
                     Pair pair2 = new Pair(joinedPlayerId, playerId, colors.stream().map(Color::revert).collect(toList()));
+                    pairs.add(pair2);
+                }
+            }
+            for (String joinedPlayer1 : joinedPlayers) {
+                isWhite = colorCounts.get(joinedPlayer1).isMoreBlacks();
+                for (String joinedPlayer2 : joinedPlayers) {
+                    List<Color> colors = new ArrayList<>();
+                    for (int i = 0; i < maxGames; i++) {
+                        colors.add(isWhite ? Color.white : Color.black);
+                        isWhite = !isWhite;
+                    }
+                    Pair pair = new Pair(joinedPlayer1, joinedPlayer2, colors);
+                    pairs.add(pair);
+                    storeColorCounts(colorCounts, joinedPlayer1, colors);
+                    Pair pair2 = new Pair(joinedPlayer2, joinedPlayer1, colors.stream().map(Color::revert).collect(toList()));
                     pairs.add(pair2);
                 }
             }
@@ -100,9 +133,14 @@ public class FixedAlternationColorAllocatingSystem implements ColorAllocatingSys
     @Override
     public Color getColor(String playerId, String opponentId, int gameNumber) {
         checkInput(playerId, opponentId, gameNumber);
-        return pairs.stream()
-                .filter(p -> p.match(playerId, opponentId))
-                .findFirst()
+        for (Pair pair : pairs) {
+            if (pair.match(playerId, opponentId)) {
+                return Optional.of(pair)
+                        .map(p -> p.getColor(gameNumber))
+                        .orElseThrow(IllegalStateException::new);
+            }
+        }
+        return Optional.<Pair>empty()
                 .map(p -> p.getColor(gameNumber))
                 .orElseThrow(IllegalStateException::new);
     }
@@ -159,6 +197,11 @@ public class FixedAlternationColorAllocatingSystem implements ColorAllocatingSys
         boolean match(String playerId, String opponentId) {
             return this.playerId.equals(playerId)
                     && this.opponentId.equals(opponentId);
+        }
+
+        @Override
+        public String toString() {
+            return MessageFormat.format("{0}->{1}: {2}", playerId, opponentId, colors);
         }
     }
 
